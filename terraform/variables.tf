438de8b1b60eb8c27cc87c91d2a7037921be48bf
Vinod Varma <95682852+Vinodvarma1999@users.noreply.github.com>
2023-02-21 12:05:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
 variable "instance_count" {
-  default = 2
+  default = 3
+}
+variable "aws_region" { 
+  type        = string
+  default     =  "us-east-1"
+  description = "The AWS region to use for resources"
 }
